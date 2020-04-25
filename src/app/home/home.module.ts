4deae148dfc908d88c2c75447d2c6bb5e338607e
Sanchit Goyal <Sanchit_Goyal@guardianindia.com>
2020-04-25 20:22:54 +0530

    updating basic nav bar

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,13 +3,24 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, NavBarComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule,
   ]
 })
 export class HomeModule { }
